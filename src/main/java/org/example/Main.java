96fcd11ddceff4c4f3e73e08b6dffae53c0b232c
Veeramani4156 <veeramani4156@gmail.com>
2023-01-31 12:07:04 +0530

    Encapsulation code by use of get set method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,25 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+class EncapsulationDemo{
+    private String name;
+    private int age;
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setName(String NewName){
+        name = NewName;
+    }
+    public void setAge(int NewAge){
+        age = NewAge;
+    }
+}
+public class Main {
+    public static void main(String[] args){
+        EncapsulationDemo obj = new EncapsulationDemo();
+        obj.setName("veeramani");
+        obj.setAge(23);
+        System.out.println(obj.getName() + ":" +obj.getAge());
     }
 }
